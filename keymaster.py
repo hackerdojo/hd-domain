343be82f1e1bd12db16107fc4d8db21dd3efdaac
Jeff Lindsay <progrium@gmail.com>
2010-02-12 03:21:27 -0800

    refactored and made a keymaster lib

---
+++ keymaster.py
@@ -0,0 +1,27 @@
+from google.appengine.api import urlfetch, memcache
+from google.appengine.ext import webapp
+import urllib
+
+_keys = {}
+
+def get(keyname):
+    return memcache.get(keyname, namespace='keymaster')
+    
+def request(keyname):
+    urlfetch.fetch('http://www.thekeymaster.org/%s' % _keys[keyname][0], method='POST', payload=urllib.urlencode({'secret': _keys[keyname][1]}))
+    
+class _Handler(webapp.RequestHandler):
+    def get(self, keyname):
+        request(keyname)
+    
+    def post(self, keyname):
+        key = self.request.get('key')
+        if key:
+            memcache.set(keyname, key, namespace='keymaster')
+            if len(_keys[keyname]) > 2:
+                _keys[keyname][2]()
+
+def Handler(keys):
+    global _keys
+    _keys = keys
+    return _Handler
--- main.py
+++ main.py
@@ -1,20 +1,3 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-
 
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import util
@@ -23,6 +6,7 @@ from django.utils import simplejson
 import gdata.apps.service
 from google.appengine.api import memcache
 import logging, urllib
+import keymaster
 
 def flatten(l):
     out = []
@@ -46,7 +30,7 @@ class UsersHandler(webapp.RequestHandler):
             self.response.out.write(simplejson.dumps(
                 [e.title.text for e in flatten([u.entry for u in client.GetGeneratorForAllUsers()])]))
         else:
-            request_token_key()
+            request_token()
             self.response.set_status(503)
             self.response.out.write("Refreshing token. Please try again.")
         
@@ -56,26 +40,29 @@ class TokenFetchHandler(webapp.RequestHandler):
         self.post()
         
     def post(self):
-        password = self.request.get('key')
-        if password:
-            client = gdata.apps.service.AppsService(domain='hackerdojo.com')
-            client.ClientLogin('jeff@hackerdojo.com', password)
-            token = client.GetClientLoginToken()
-            if not memcache.add('token', token, 3600*24):
-                logging.error("Memcache set failed.")
-            else:
-                logging.info('Token fetched: %s' % token)
-        else:
-            request_token_key()
+        request_token()
+        self.response.out.write(memcache.get('token'))
             
-def request_token_key():
-    urlfetch.fetch('http://www.thekeymaster.org/f052e1e1640752cbd83630226dfe311b', method='POST', payload=urllib.urlencode({'secret': '4awrfzvz'}))
+def request_token():
+    if keymaster.get('domain-pass'):
+        client = gdata.apps.service.AppsService(domain='hackerdojo.com')
+        client.ClientLogin('jeff@hackerdojo.com', keymaster.get('domain-pass'))
+        token = client.GetClientLoginToken()
+        if not memcache.set('token', token, 3600*24):
+            logging.error("Memcache set failed.")
+        else:
+            logging.info('Token fetched: %s' % token)
+    else:
+        keymaster.request('domain-pass')
 
 def main():
     application = webapp.WSGIApplication([
         ('/', MainHandler),
         ('/users', UsersHandler),
         ('/token/fetch', TokenFetchHandler),
+        ('/key/(.+)', keymaster.Handler({
+            'domain-pass': ('6f7e71752e29e6d4b4e64daceb2a7348', '1iuy010y', request_token),
+            })),
         ],debug=True)
     util.run_wsgi_app(application)
 
