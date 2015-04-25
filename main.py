7936b4ac53c7020aab16b2df5852643addf7d63b
Daniel Petti <djpetti@gmail.com>
2015-04-25 14:57:16 -0700

    Add authentication requirements for API.
    You now have to be a known app in order to use it.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import base64
 import json
 import logging
+import os
 import sys
 import time
 
@@ -17,80 +18,116 @@ DOMAIN_ACCOUNT = 'api@hackerdojo.com'
 
 
 class MainHandler(webapp2.RequestHandler):
-    def get(self):
-        self.response.out.write("Try /users or /groups ... %s" % users.get_current_user())
+  def get(self):
+    self.response.out.write("Try /users or /groups ... %s" % users.get_current_user())
 
 
 class BaseHandler(webapp2.RequestHandler):
-    def domain(self):
-        return Domain(DOMAIN_ACCOUNT.split('@')[1])
+  # GAE apps that are allowed to use this API.
+  _AUTHORIZED_APPS = ("hd-signup-hrd", "signup-dev", "hd-events")
+
+  """ Meant to be used as a decorator for functions that should only be accessed
+  by authorized people and apps. The wrapped function checks the authorization,
+  and fails with an error if it is incorrect.
+  function: The function we are decorating.
+  Returns: A wrapped version of the function that checks authorization. """
+  @classmethod
+  def restricted(cls, function):
+    """ The wrapper function. """
+    def wrapper(self, *args, **kwargs):
+      app_id = self.request.headers.get('X-Appengine-Inbound-Appid', None)
+      logging.debug("Request from app '%s'." % (app_id))
+
+      # Make an automatic exception if we are running on the local dev server.
+      if "Development" in os.environ["SERVER_SOFTWARE"]:
+        logging.info("Dev server detected. Not restricting access.")
+      elif app_id not in self._AUTHORIZED_APPS:
+        # Unauthorized.
+        logging.warning("Denying request from unauthorized source.")
+        self.abort(403)
+
+      return function(self, *args, **kwargs)
+
+    return wrapper
+
+  def domain(self):
+    return Domain(DOMAIN_ACCOUNT.split('@')[1])
 
 
 class GroupsHandler(BaseHandler):
-    def get(self):
-        groups = self.domain().list_groups()
-        self.response.out.write(json.dumps(groups))
+  @BaseHandler.restricted
+  def get(self):
+    groups = self.domain().list_groups()
+    self.response.out.write(json.dumps(groups))
 
 
 class GroupHandler(BaseHandler):
-    def get(self, group_id):
-        group = self.domain().get_group(group_id)
-        self.response.out.write(json.dumps(group))
+  @BaseHandler.restricted
+  def get(self, group_id):
+    group = self.domain().get_group(group_id)
+    self.response.out.write(json.dumps(group))
 
 
 class UsersHandler(BaseHandler):
-    def get(self):
-        users_str = memcache.get('users_str')
-        if not users_str:
-            users = self.domain().list_users()
-            users_str = json.dumps(users)
-            memcache.set('users_str', users_str)
-        self.response.out.write(users_str)
+  @BaseHandler.restricted
+  def get(self):
+    users_str = memcache.get('users_str')
+    if not users_str:
+      users = self.domain().list_users()
+      users_str = json.dumps(users)
+      memcache.set('users_str', users_str)
+    self.response.out.write(users_str)
 
-    def post(self):
-        if self.secure():
-            memcache.delete('users_str')
+  @BaseHandler.restricted
+  def post(self):
+    memcache.delete('users_str')
 
-            user = self.domain().add_user(
-                username    = self.request.get('username'),
-                password    = self.request.get('password'),
-                first_name  = self.request.get('first_name'),
-                last_name   = self.request.get('last_name'))
-            self.response.out.write(json.dumps(user))
+    user = self.domain().add_user(
+        username    = self.request.get('username'),
+        password    = self.request.get('password'),
+        first_name  = self.request.get('first_name'),
+        last_name   = self.request.get('last_name'))
+    self.response.out.write(json.dumps(user))
 
 
 class UsersNoCacheHandler(BaseHandler):
-    def get(self):
-        users = self.domain().list_users()
-        users_str = json.dumps(users)
-        memcache.set('users_str', users_str)
-        self.response.out.write(users_str)
+  @BaseHandler.restricted
+  def get(self):
+    users = self.domain().list_users()
+    users_str = json.dumps(users)
+    memcache.set('users_str', users_str)
+    self.response.out.write(users_str)
 
 
 class SuspendHandler(BaseHandler):
-    def get(self, username):
-        self.post(username)
+  @BaseHandler.restricted
+  def get(self, username):
+    self.post(username)
 
-    def post(self, username):
-        memcache.delete('users_str')
-        user = self.domain().suspend_user(username)
-        self.response.out.write(json.dumps(user))
+  @BaseHandler.restricted
+  def post(self, username):
+    memcache.delete('users_str')
+    user = self.domain().suspend_user(username)
+    self.response.out.write(json.dumps(user))
 
 
 class RestoreHandler(BaseHandler):
-    def get(self, username):
-        self.post(username)
+  @BaseHandler.restricted
+  def get(self, username):
+    self.post(username)
 
-    def post(self, username):
-        memcache.delete('users_str')
-        user = self.domain().restore_user(username)
-        self.response.out.write(json.dumps(user))
+  @BaseHandler.restricted
+  def post(self, username):
+    memcache.delete('users_str')
+    user = self.domain().restore_user(username)
+    self.response.out.write(json.dumps(user))
 
 
 class UserHandler(BaseHandler):
-    def get(self, username):
-        user = self.domain().get_user(username)
-        self.response.out.write(json.dumps(user))
+  @BaseHandler.restricted
+  def get(self, username):
+    user = self.domain().get_user(username)
+    self.response.out.write(json.dumps(user))
 
 
 app = webapp2.WSGIApplication([
